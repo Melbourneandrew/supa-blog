d1049862ba2cd00e28f85ad84f59c7ca58bd1ec1
Andrew Melbourne <melbourneandrew@gmail.com>
2024-12-14 22:42:10 -0600

    title and desc from .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Supa Blog",
-  description: "A simple blog starter kit built with Supabase and Next.js",
+  title: process.env.NEXT_PUBLIC_BLOG_TITLE || "Supa Blog",
+  description: process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "A simple blog starter kit built with Supabase and Next.js",
 };
 
 export default function RootLayout({
